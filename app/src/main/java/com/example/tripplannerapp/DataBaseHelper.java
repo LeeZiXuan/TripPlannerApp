204bb4f08d0d35c3252caefc7dd7aaacf015c46d
LeeZiXuan <92352011+LeeZiXuan@users.noreply.github.com>
2022-06-08 01:12:44 +0800

    Lee

---
+++ app/src/main/java/com/example/tripplannerapp/DataBaseHelper.java
@@ -0,0 +1,37 @@
+package com.example.tripplannerapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    public static final String USER_TABLE = "USER_TABLE";
+    public static final String ID = "ID";
+    public static final String USER_NAME = "USER_NAME";
+    public static final String USER_AGE = "USER_AGE";
+    public static final String USER_PASS = "USER_PASS";
+    public static final String USER_PHONE = "USER_PHONE";
+    public static final String USER_GENDER = "USER_GENDER";
+    public static final String USER_EMAIL = "USER_EMAIL";
+    public static final String USER_ADDRESS = "USER_ADDRESS";
+    public static final String USER_BIRTH = "USER_BIRTH";
+
+    public DataBaseHelper(@Nullable Context context) {
+        super(context, "TripPlannerApp.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTableStatement = "CREATE TABLE " + USER_TABLE + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + USER_NAME + " TEXT, " + USER_AGE + " INT, " + USER_PASS + " TEXT, " + USER_PHONE + " TEXT, " + USER_GENDER + " TEXT, " + USER_EMAIL + " TEXT, " + USER_ADDRESS + " TEXT, " + USER_BIRTH + " DATE)";
+
+        sqLiteDatabase.execSQL(createTableStatement);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
--- app/src/main/java/com/example/tripplannerapp/LoginActivity.java
+++ app/src/main/java/com/example/tripplannerapp/LoginActivity.java
@@ -2,13 +2,34 @@ package com.example.tripplannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomePage();
+
+            }
+        });
+
+    }
+
+    public void openHomePage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
