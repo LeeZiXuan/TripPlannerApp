93f7bd980962df0cd7751567d44311f77a05ea65
LeeZiXuan <92352011+LeeZiXuan@users.noreply.github.com>
2022-06-08 02:58:59 +0800

    Lee

--- app/src/main/java/com/example/tripplannerapp/DataBaseHelper.java
+++ app/src/main/java/com/example/tripplannerapp/DataBaseHelper.java
@@ -14,13 +14,13 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     public static final String USER_TABLE = "USER_TABLE";
     public static final String ID = "ID";
     public static final String USER_NAME = "USER_NAME";
-    public static final String USER_AGE = "USER_AGE";
+    //public static final String USER_AGE = "USER_AGE";
     public static final String USER_PASS = "USER_PASS";
-    public static final String USER_PHONE = "USER_PHONE";
-    public static final String USER_GENDER = "USER_GENDER";
+    //public static final String USER_PHONE = "USER_PHONE";
+    //public static final String USER_GENDER = "USER_GENDER";
     public static final String USER_EMAIL = "USER_EMAIL";
-    public static final String USER_ADDRESS = "USER_ADDRESS";
-    public static final String USER_BIRTH = "USER_BIRTH";
+    //public static final String USER_ADDRESS = "USER_ADDRESS";
+    //public static final String USER_BIRTH = "USER_BIRTH";
 
     public DataBaseHelper(Context context) {
         super(context, "TripPlannerApp.db", null, 1);
@@ -29,7 +29,8 @@ public class DataBaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("create table login(email TEXT primary key, password TEXT, username TEXT)");
-        String createTableStatement = "CREATE TABLE " + USER_TABLE + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + USER_NAME + " TEXT, " + USER_AGE + " INT, " + USER_PASS + " TEXT, " + USER_PHONE + " TEXT, " + USER_GENDER + " TEXT, " + USER_EMAIL + " TEXT, " + USER_ADDRESS + " TEXT, " + USER_BIRTH + " DATE)";
+        //String createTableStatement = "CREATE TABLE " + USER_TABLE + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + USER_NAME + " TEXT, " + USER_AGE + " INT, " + USER_PASS + " TEXT, " + USER_PHONE + " TEXT, " + USER_GENDER + " TEXT, " + USER_EMAIL + " TEXT, " + USER_ADDRESS + " TEXT, " + USER_BIRTH + " DATE)";
+        String createTableStatement = "CREATE TABLE " + USER_TABLE + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + USER_NAME + " TEXT, " + USER_PASS + " TEXT,  " + USER_EMAIL + " TEXT)";
 
         sqLiteDatabase.execSQL(createTableStatement);
 
