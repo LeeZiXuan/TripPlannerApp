1a10309e2d2285341ee591f2ac7293b5067f9920
LeeZiXuan <92352011+LeeZiXuan@users.noreply.github.com>
2022-06-08 01:15:34 +0800

    Lee

--- app/src/main/java/com/example/tripplannerapp/LoginActivity.java
+++ app/src/main/java/com/example/tripplannerapp/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login = (Button) findViewById(R.id.loginBtn);
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
