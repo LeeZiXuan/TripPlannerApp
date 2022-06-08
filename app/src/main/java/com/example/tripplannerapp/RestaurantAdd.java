0f29e81756665635d627b3dccc22174a9fd29bce
mk <meikeigoh@gmail.com>
2022-06-08 18:03:45 +0800

    testing

---
+++ app/src/main/java/com/example/tripplannerapp/RestaurantAdd.java
@@ -0,0 +1,13 @@
+package com.example.tripplannerapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class RestaurantAdd {
+}
